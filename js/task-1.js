bdf0247d1181607eaef11cedc8dd85393a8c4d26
Kostya Tkachenko <tkostyantin@gmail.com>
2025-07-14 07:35:54 +0300

    hw-JS-7
    Додав параметри CSS.

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,12 @@
+const categoryItems = document.querySelectorAll('.item');
+
 categoryItems.forEach((item, index) => {
-    
     const heading = item.querySelector('h2');
     const categoryName = heading.textContent;
-    
+
     const nestedItems = item.querySelectorAll('ul li');
     const itemCount = nestedItems.length;
-    
+
     console.log(`Category #${index + 1}: ${categoryName}`);
     console.log(`Elements: ${itemCount}`);
-    });
+});
